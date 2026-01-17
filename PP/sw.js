df846e15ef1f8a6fa0a11177c0afb7b62f58ecda
Adjie312 <ajitegar7758@gmail.com>
2026-01-17 14:38:58 +0700

    Update sw.js

--- PP/sw.js
+++ PP/sw.js
@@ -7,8 +7,7 @@ const ASSETS_TO_CACHE = [
     '/skrip.js',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
     'https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300;400;500;600;700&family=Inter:wght@300;400;500;600;700&display=swap'
-];
-
+]
 // Install event - cache assets
 self.addEventListener('install', event => {
     event.waitUntil(
